2c9f42dccb76bf37c163c40128c67dba06e3dd60
Leonardo Barrientos C <lbarrientos@bctic.net>
2015-05-12 17:38:30 -0300

    SPANISH NO PORTAL NO NEWS

--- manifest.php
+++ manifest.php
@@ -4,9 +4,9 @@ $manifest = array(
   'description' => 'Spanish (Spain) Language Pack',
 	'type' => 'langpack',
 	'is_uninstallable' => 'Yes',
-	'version' => '6.5',
+	'version' => '1.2',
 	'acceptable_sugar_flavors' => array ( 0 => "CE", 1 => "PRO", 2 => "CORP", 3 => "ENT", 4 => "ULT"),
-	'author' => 'BTACTIC S.C.C.L.',
+	'author' => 'BTACTIC S.C.C.L. & Leonardo Barrientos C',
 	'acceptable_sugar_versions' => array ( "exact_matches" => array (), "regex_matches" => array (	0 => "6.5.[0-9][a-z]?" ,
         	1 => "6.4.[0-9][a-z]?" ,
         	2 => "6.3.[0-9][a-z]?" ), ),
@@ -17,9 +17,9 @@ $installdefs = array(
 	'id'=> 'es_es',
 	'image_dir'=>'<basepath>/images',
 	'copy' => array(
-	array('from'=> '<basepath>/include','to'=> 'include',),
-	array('from'=> '<basepath>/modules','to'=> 'modules'),
-	array('from'=> '<basepath>/install','to'=> 'install'),
-	array('from'=> '<basepath>/portal','to'=> 'portal'))
+	  array('from'=> '<basepath>/include','to'=> 'include',),
+	  array('from'=> '<basepath>/modules','to'=> 'modules'),
+	  array('from'=> '<basepath>/install','to'=> 'install')
+	)
 );
 ?>
